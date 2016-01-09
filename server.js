db5b1ed90ce718913080d6b15def2db4efce204c
jckhang <yz3079@nyu.edu>
2016-01-09 09:41:16 +0800

    update

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp.js')
+
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp.js')
+
+
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp.js')
+
+
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});var express = require('express');
+var app = express();
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp.js')
+
+
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
