13b838ac6424c3b4ed939f0b464d73bd888fd376
jckhang <yz3079@nyu.edu>
2016-01-09 09:52:10 +0800

    update

--- server.js
+++ server.js
@@ -1,41 +1,7 @@
 'use strict';
 
-'use strict';
-
+// Call the package we need.
 var express = require('express');
 var app = express();
 var routes = require('./app/routes/index.js');
-var api = require('./app/api/timestamp.js')
-
-'use strict';
-
-var express = require('express');
-var app = express();
-var routes = require('./app/routes/index.js');
-var api = require('./app/api/timestamp.js')
-
-
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});'use strict';
-
-var express = require('express');
-var app = express();
-var routes = require('./app/routes/index.js');
-var api = require('./app/api/timestamp.js')
-
-
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});var express = require('express');
-var app = express();
-var routes = require('./app/routes/index.js');
-var api = require('./app/api/timestamp.js')
-
-
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+var api = require('./app/api/timestamp.js');
