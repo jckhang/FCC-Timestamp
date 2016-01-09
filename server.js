8a71e048d24c6904618a3d0185c9db136068c3f7
jckhang <yz3079@nyu.edu>
2016-01-09 15:13:13 +0800

    update

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ var api = require('./app/api/timestamp.js');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 var port = process.env.PORT || 8080;        // set our port
